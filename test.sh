ee8cccfa05bf5661010f20268e64a8de08c13841
mayat696 <mayathompson696@gmail.com>
2025-08-28 16:02:31 -0400

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Penetration Test Proof of Concept
+# This script demonstrates successful unrestricted file upload
+# Created for authorized security testing only
+
+echo "File upload vulnerability confirmed!"
